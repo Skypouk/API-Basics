f34d5c9d2c6df7d4c04fb90d71c6b374974fb2af
skypouk <bentaher.email@gmail.com>
2024-06-26 15:07:36 +0200

    Add auth0 for backend authentication & authorization (using scopes) if needed

--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,30 @@
 from fastapi import FastAPI, Request
-
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers.users import user_router
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(root_path="/api")
+
+origins = [
+    "http://localhost:4040",
+    "https://skypouk.org"
+]
+
+# Add CORS middleware to the FastAPI application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(user_router)
 
 
 @app.get("/")
 def root(request: Request):
-    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
+    return {"data": "Welcome to public page", "root_path": request.scope.get("root_path")}
--- api/app/routers/users.py
+++ api/app/routers/users.py
@@ -1,11 +1,14 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Security
 from sqlalchemy.orm import Session
 
 from app.crud.users import UserCRUD
 from app.dependencies import get_db
 from app.schemas import users
+from app.auth.auth0_service import AuthService
+
+auth = AuthService()
 
 user_router = APIRouter(
     prefix="/users",
@@ -14,21 +17,21 @@ user_router = APIRouter(
 )
 
 
-@user_router.post("/users/", response_model=users.User)
-def create_user(user: users.UserCreate, db: Session = Depends(get_db)):
+@user_router.post("/", response_model=users.User)
+def create_user(user: users.UserCreate, db: Session = Depends(get_db), auth_result: str = Security(auth.verify, scopes=[])):
     db_user = UserCRUD.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return UserCRUD.create_user(db=db, user=user)
 
 
-@user_router.get("/users/", response_model=List[users.User])
+@user_router.get("/", response_model=List[users.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = UserCRUD.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@user_router.get("/users/{user_id}", response_model=users.User)
+@user_router.get("/{user_id}", response_model=users.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = UserCRUD.get_user(db, user_id=user_id)
     if db_user is None:
